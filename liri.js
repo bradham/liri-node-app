6a454631d2b5dddad6c9615a32788301f18a3dbc
bradham <bradham@users.noreply.github.com>
2019-09-28 19:31:36 -0500

    Added switch check for correct input

--- liri.js
+++ liri.js
@@ -14,10 +14,40 @@ var spotify = require("node-spotify-api");
 
 var keys = require("./keys.js");
 
-var spotifyKey = new Spotify(keys.spotify);
+//var spotifyKey = new Spotify(keys.spotify);
+
+//Get the command the user wrote
+var command = process.argv[2];
+console.log("Command is " + command);
+
+//Get the name of the artist or song or movie to find
+var name = process.argv[3];
+
+switch (command) {
+    case "concert-this":
+        //code
+        break;
+
+    case "spotify-this-song":
+        //code
+        break;
+
+    case "movie-this":
+        //code
+        break;
+
+    case "do-what-it-says":
+        //code
+        break;
+
+    default: {
+        console.log("Please input a correct command");
+    }
+
+}
 
 // concert-this
-/* 
+/*
 node liri.js concert-this <artist/band name here>
 
 This will search the Bands in Town Artist Events API
@@ -28,6 +58,8 @@ Name of the venue
 Venue location
 Date of the Event (use moment to format this as "MM/DD/YYYY")
 */
+// Store all of the arguments in an array
+
 
 // spotify-this-song
 
